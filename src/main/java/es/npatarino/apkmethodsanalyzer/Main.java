3411fc8a90b8e9ae879a8e53cfb0bf94f70f947f
Nicolás Patarino <npatarino@idealista.com>
2016-05-15 18:01:43 +0200

    Load config from config/config.json if it exists

--- src/main/java/es/npatarino/apkmethodsanalyzer/Main.java
+++ src/main/java/es/npatarino/apkmethodsanalyzer/Main.java
@@ -22,10 +22,7 @@ import es.npatarino.apkmethodsanalyzer.dex.DexMethodCounts;
 import es.npatarino.apkmethodsanalyzer.files.CollectFileNames;
 import es.npatarino.apkmethodsanalyzer.output.GenerateHtmlOutput;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
 
@@ -33,21 +30,17 @@ public class Main {
     private int maxDepth = Integer.MAX_VALUE;
     private DexMethodCounts.Filter filter = DexMethodCounts.Filter.ALL;
     private int minPercentageToPrint = 1;
+    private String[] inputFileNames;
 
     public static void main(String[] args) {
         Main main = new Main();
-        main.run(new String[]{"idealista-production-release-7.0.3.apk"});
+        main.run(args);
     }
 
     void run(String[] args) {
         try {
-            Config config = new Config();
-            File configFile = new File("config/config.json");
-            if (configFile.exists()) {
-                BufferedReader reader = new BufferedReader(new FileReader(configFile));
-                config = new Gson().fromJson(reader, Config.class);
-            }
-            String[] inputFileNames = new String[]{"idealista-production-release-7.0.3.apk"};
+            Config config = parseConfigJson();
+            parseArgs(args);
             int overallCount = 0;
             CollectFileNames collectFileNames = new CollectFileNames(inputFileNames);
             for (String fileName : collectFileNames.invoke()) {
@@ -70,14 +63,51 @@ public class Main {
         }
     }
 
+    private Config parseConfigJson() throws FileNotFoundException {
+        Config config = new Config();
+        File configFile = new File("config/config.json");
+        if (configFile.exists()) {
+            BufferedReader reader = new BufferedReader(new FileReader(configFile));
+            config = new Gson().fromJson(reader, Config.class);
+        }
+        return config;
+    }
+
+    private void parseArgs(String[] args) {
+        int idx;
+        for (idx = 0; idx < args.length; idx++) {
+            String arg = args[idx];
+            if (arg.equals("--") || !arg.startsWith("--")) {
+                break;
+            } else if (arg.startsWith("--package-filter=")) {
+                packageFilter = arg.substring(arg.indexOf('=') + 1);
+            } else if (arg.startsWith("--max-depth=")) {
+                maxDepth = Integer.parseInt(arg.substring(arg.indexOf('=') + 1));
+            } else if (arg.startsWith("--filter=")) {
+                filter = Enum.valueOf(
+                        DexMethodCounts.Filter.class,
+                        arg.substring(arg.indexOf('=') + 1).toUpperCase());
+            } else {
+                System.err.println("Unknown option '" + arg + "'");
+                throw new es.npatarino.apkmethodsanalyzer.Main.UsageException();
+            }
+        }
+
+        int fileCount = args.length - idx;
+        if (fileCount == 0) {
+            throw new es.npatarino.apkmethodsanalyzer.Main.UsageException();
+        }
+        inputFileNames = new String[fileCount];
+        System.arraycopy(args, idx, inputFileNames, 0, fileCount);
+    }
 
     private void usage() {
         System.err.print(
-                "DEX per-package/class method counts v1.5\n" +
+                "APK per-package method counts v1.0\n" +
                         "Usage: apk-methods-analyzer [options] <file.{dex,apk,jar,directory}> ...\n" +
                         "Options:\n" +
-                        "  --include-classes\n" +
                         "  --package-filter=com.foo.bar\n" +
+                        "  --max-depth=N\n" +
                         "  --filter=ALL|DEFINED_ONLY|REFERENCED_ONLY\n"
         );
     }
